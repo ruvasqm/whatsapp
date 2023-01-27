79648e8bf41ee3e522e02685cf866d331c4f8939
Ruben Vasquez <ruvasqm@gmail.com>
2023-01-27 16:25:10 -0400

    feat: connect validate to function

--- src/main.js
+++ src/main.js
@@ -3,29 +3,21 @@ const redirect2WS = (number) => {
     window.location.href = `https://wa.me/${number}`
 }
 
-const validateNumber = (number) => {
-    // test for us number (numbers only, no spaces, no dashes, no parentheses, no plus sign) including country code
-    const usNumber = /^(\+?1)?[2-9]\d{2}[2-9](?!11)\d{6}$/;
-    // test for latin american number (numbers only, no spaces, no dashes, no parentheses, no plus sign) including country code
-    // countries: Argentina, Bolivia, Brazil, Chile, Colombia, Costa Rica, Dominican Republic, Ecuador, El Salvador, Guatemala, Honduras, Mexico, Nicaragua, Panama, Paraguay, Peru, Puerto Rico, Uruguay, Venezuela
-    const latinAmericanNumber = /^(\+?5)?[2-9]\d{2}[2-9](?!11)\d{6}$/;
-    // test for european number (numbers only, no spaces, no dashes, no parentheses, no plus sign) including country code
-    // countries: Austria, Belgium, Bulgaria, Croatia, Cyprus, Czech Republic, Denmark, Estonia, Finland, France, Germany, Greece, Hungary, Iceland, Ireland, Italy, Latvia, Liechtenstein, Lithuania, Luxembourg, Malta, Monaco, Netherlands, Norway, Poland, Portugal, Romania, San Marino, Slovakia, Slovenia, Spain, Sweden, Switzerland, United Kingdom
-    const europeanNumber = /^(\+?3)?[2-9]\d{2}[2-9](?!11)\d{6}$/;
-    return usNumber.test(number) || latinAmericanNumber.test(number) || europeanNumber.test(number)
+const validateNumber = async (number) => {
+    const result = await fetch('/.netlify/functions/validateNumber', {
+        method: 'POST',
+        body: JSON.stringify({ number: number })
+    }
+    )
+    const data = await result.json()
+    return data.valid;
 }
+
 [ "input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop", "focusout" ].forEach(function(event) {
 
     document.getElementById('number').addEventListener(event, (e) => {
         // remove all non-numeric characters
         e.target.value = e.target.value.replace(/\D/g, '')
-        if (validateNumber(e.target.value)) {
-            document.getElementById('submit').disabled = false
-        }
-        else {
-            document.getElementById('submit').disabled = true
-        }
-
     })
 })
 
