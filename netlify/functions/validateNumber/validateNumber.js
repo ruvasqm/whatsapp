47e722bfebe1a44f41488683962c60d4cbd87226
Ruben Vasquez <ruvasqm@gmail.com>
2023-01-27 11:51:24 -0400

    feat: add number validation function

--- netlify/functions/validateNumber/validateNumber.js
+++ netlify/functions/validateNumber/validateNumber.js
@@ -1,43 +1,41 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
-const https = require('https')
+const https = require('https');
 const handler = async (event) => {
   try {
     const number = (await JSON.parse(event.body)).number;
     // url = 'https://api.apilayer.com/number_verification/validate?number=14158586273'
-    // headers = { apikey: process.env.API_KEY }
-    const headers = { 'Content-Type': 'application/json', apikey: process.env.API_KEY }
-    https.get({
-        hostname: 'api.apilayer.com',
-        path: `/number_verification/validate?number=${number}`,
-        headers: headers,
-        method: 'GET',
-    }, (res) => {
-        let data = '';
-        res.on('data', (chunk) => {
-            data += chunk;
+    const headers = { "apikey": process.env.API_KEY }
+    let result = await new Promise((resolve, reject) => {
+        const req = https.request({
+            hostname: 'api.apilayer.com',
+            path: `/number_verification/validate?number=${number}`,
+            method: 'GET',
+            headers: headers
+        }, (res) => {
+            let data = '';
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+            res.on('end', () => {
+                resolve(data);
+            });
         });
-        res.on('end', () => {
-            data = JSON.parse(data);
-            if(data.valid) {
-                return {
-                    statusCode: 200,
-                    body: JSON.stringify({ valid: true })
-                }
-            }
-            else {
-                return {
-                    statusCode: 200,
-                    body: JSON.stringify({ valid: false })
-                }
-            }
+        req.on('error', (error) => {
+            reject(error);
         });
-    }).on("error", (err) => {
-        console.log("Error: " + err.message);
+        req.end();
     });
+    return {
+        statusCode: 200,
+        body: result
+    }
+
     } catch (err) {
-        return { statusCode: 500, body: err.toString() }
+        const body = { error: err.toString() , valid: false}
+        return { statusCode: 500, body: JSON.stringify(body) }
     }
 
 }
 
 module.exports = { handler }
+
